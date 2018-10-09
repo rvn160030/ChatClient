8e1215328dcd79a8454420bbba8a53c63716a161
Vincent Noble <vincentnoble@cometnet-10-21-41-237.utdallas.edu>
2018-10-09 08:22:40 -0500

    Initial commit

---
+++ src/app/security.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SecurityService {
+
+  constructor() { }
+
+  // This is where the encryption/decryption stuff could go 
+}
